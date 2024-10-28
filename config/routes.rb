a96187a2100e3e4560bcb19f293f150236bfce7d
david127 <oreanticonasteam@gmail.com>
2024-10-27 20:11:50 -0500

    Primer commit: subir el proyecto CRUD

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  resources :comentarios
+  resources :categoria
+  resources :articulos
+  resources :usuarios
+  
+  root "usuarios#index"
+
+  
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+end
